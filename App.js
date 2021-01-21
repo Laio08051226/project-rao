41684959643dd58c1af182f17a94d45f6c9d33da
Laio08051226 <69252600+Laio08051226@users.noreply.github.com>
2021-01-21 12:50:43 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 // import { renderRoutes, matchRoutes } from 'react-router-config'
 // import routes from './routes'
 
 //基本排版元件
 import Header from './components/main/Header'
-import MyFooter from './components/main/MyFooter/MyFooter'
+// import MyFooter from './components/main/MyFooter/MyFooter'
 //頁面
 import Home from './pages/Home'
 import CourseGuide from './pages/CourseGuide'
@@ -13,6 +13,10 @@ import CourseSearch from './pages/CourseSearch'
 import CourseInfo from './pages/CourseInfo'
 
 function App() {
+  // const { finalType, setFinalType } = useState()
+  // useEffect(() => {
+
+  // },[finalType])
   return (
     <Router>
       <>
@@ -22,10 +26,12 @@ function App() {
             <Home />
           </Route>
           <Route path="/course" exact>
+            {/* <CourseGuide setFinalType={setFinalType} /> */}
             <CourseGuide />
           </Route>
-          <Route path="/course/search/:type?" exact>
-            <CourseSearch />
+          <Route path="/course/search/" exact>
+            {/* <CourseSearch type={finalType} /> */}
+            <CourseSearch Guide_type={'succlent'} />
           </Route>
           <Route path="/course/:id" exact>
             <CourseInfo />
